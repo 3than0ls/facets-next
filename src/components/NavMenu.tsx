dbb9e8d80018c7899f62e3f95736417b5c695d9c
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-06-30 12:51:35 -0700

    Added login link to NavMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,18 +3,21 @@ import * as NM from '@radix-ui/react-navigation-menu';
 export default function NavMenu() {
     return <NM.Root className='bg-accent text-secondary w-full py-4'>
         <NM.List className='flex justify-end w-full text-white'>
-            <NM.Item className='px-8 mr-auto text-2xl font-bold'>
-                Facets
+            <NM.Item className='px-8 mr-auto text-2xl font-bold hover:text-primary transition-colors duration-200'>
+                <NM.Link href="/">Facets</NM.Link>
             </NM.Item>
 
-            <NM.Item className='my-auto mx-8'>
+            <NM.Item className='my-auto mx-8 hover:text-primary transition-colors duration-200'>
                 <NM.Link href="https://www.google.com">About</NM.Link>
             </NM.Item>
 
-            <NM.Item className='my-auto mx-8'>
+            <NM.Item className='my-auto mx-8 hover:text-primary transition-colors duration-200'>
                 <NM.Link href="https://www.google.com">Github</NM.Link>
             </NM.Item>
 
+            <NM.Item className='my-auto mx-8 hover:text-primary transition-colors duration-200'>
+                <NM.Link href="/login">Login</NM.Link>
+            </NM.Item>
         </NM.List>
 
         <NM.Viewport />
