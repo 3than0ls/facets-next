b88a6627941149bfbc41f55fadcfd2e817efc2c0
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-07-08 17:32:16 -0700

    Turned messagebox into a form element

--- src/components/chat/MessageBox.tsx
+++ src/components/chat/MessageBox.tsx
@@ -29,7 +29,10 @@ const MessageBox = () => {
 
     const postMessage = usePostMessage()
 
-    const sendMessage = async () => {
+    const sendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        event.stopPropagation()
+
         // this should be handled client side by just preventing clicking
         if (!user) {
             throw new Error('Not signed in')
@@ -37,25 +40,28 @@ const MessageBox = () => {
 
         const result = await postMessage({ text: message })
 
-        console.log(result)
+        // should probably check if result OK
+        setMessage('')
     }
 
-    // TODO: turn this into a form, have an onSubmit and a name for the input
-
     return (
-        <div className="flex flex-row h-24 w-full border-accent focus:outline-accent p-2">
+        <form
+            onSubmit={sendMessage}
+            className="flex flex-row h-24 w-full border-accent focus:outline-accent p-2"
+        >
             <input
+                name="message"
+                value={message}
                 onChange={onChange}
                 className="bg-primary w-full h-full rounded-l-xl p-2 outline-none hover:outline-none border-2 border-transparent hover:border-accent focus:border-accent transition-colors duration-200"
             />
             <button
-                type="button"
-                onClick={sendMessage}
+                type="submit"
                 className="bg-accent h-full min-w-8 my-auto flex rounded-r-md"
             >
                 <TbSend color="white" className="m-auto" />
             </button>
-        </div>
+        </form>
     )
 }
 
