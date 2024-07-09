d2124c4329cf4bd1aff9cbd0746dd3fc25f002e0
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-07-08 21:02:08 -0700

    Resolved peer dep issue by just deleting eslint-plugin-unused-imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,13 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import unusedImports from 'eslint-plugin-unused-imports'
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     {
-        plugins: {
-            'unused-imports': unusedImports,
-        },
-    },
-    {
         rules: {
             camelcase: 'error',
-            '@typescript-eslint/no-unused-vars': 'off',
-            'unused-imports/no-unused-imports': 'warn',
-            'unused-imports/no-unused-vars': [
-                'warn',
-                {
-                    vars: 'all',
-                    varsIgnorePattern: '^_',
-                    args: 'after-used',
-                    argsIgnorePattern: '^_',
-                },
-            ],
+            '@typescript-eslint/no-unused-vars': 'warn',
         },
     },
     {
