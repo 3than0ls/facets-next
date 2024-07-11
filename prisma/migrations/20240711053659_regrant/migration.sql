331709de18725df7e85a2553f7f3ca517310951d
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-07-10 22:50:12 -0700

    implemented realtime update of messages

---
+++ prisma/migrations/20240711053659_regrant/migration.sql
@@ -0,0 +1,12 @@
+-- regrant default public grant schemas that were wiped by prisma
+-- https://supabase.com/partners/integrations/prisma > Troubleshooting > Missing grants
+
+grant usage on schema public to postgres, anon, authenticated, service_role;
+
+grant all privileges on all tables in schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all functions in schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all sequences in schema public to postgres, anon, authenticated, service_role;
+
+alter default privileges in schema public grant all on tables to postgres, anon, authenticated, service_role;
+alter default privileges in schema public grant all on functions to postgres, anon, authenticated, service_role;
+alter default privileges in schema public grant all on sequences to postgres, anon, authenticated, service_role;
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Message from './Message'
+import { User } from '@supabase/supabase-js'
 import ChatScroll from './ChatScroll'
+import { createClient } from '@/utils/supabase/client'
 
 type MessagesProps = {
     serverProps: {
@@ -13,6 +15,22 @@ type MessagesProps = {
 const Messages = ({
     serverProps: { serverMessageComponents },
 }: MessagesProps) => {
+    const supabase = createClient()
+
+    const [user, setUser] = useState<User | null | undefined>(undefined)
+
+    useEffect(() => {
+        const { data: authListener } = supabase.auth.onAuthStateChange(
+            (event, session) => {
+                setUser(session?.user ?? null)
+                console.log(event)
+            },
+        )
+        return () => {
+            authListener.subscription.unsubscribe()
+        }
+    }, [])
+
     const [messageComponents, setMessageComponents] = useState([
         ...serverMessageComponents,
     ])
@@ -22,6 +40,37 @@ const Messages = ({
         setMessageComponents([...messageComponents, newMessage].slice(0, 50))
         console.log('fetching more data', serverMessageComponents.length)
     }
+
+    // https://www.youtube.com/watch?v=YR-xP6PPXXA
+    useEffect(() => {
+        const channel = supabase
+            .channel('supabase_realtime')
+            .on(
+                'postgres_changes',
+                {
+                    event: 'INSERT',
+                    schema: 'public',
+                    table: 'Message',
+                },
+                (payload) => {
+                    const newMessage = (
+                        <Message
+                            key={payload.new.id}
+                            text={payload.new.text}
+                            selfAuthor={payload.new.userId === user?.id}
+                        />
+                    )
+                    setMessageComponents(
+                        [newMessage, ...messageComponents].slice(0, 50),
+                    )
+                },
+            )
+            .subscribe()
+
+        return () => {
+            supabase.removeChannel(channel)
+        }
+    }, [supabase, user])
 
     return (
         <ChatScroll className="w-full h-full overflow-auto" loadMore={loadMore}>
