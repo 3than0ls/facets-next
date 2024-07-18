d11bd01ad35342aa214dab31f4158c962b288593
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-07-17 22:21:34 -0700

    fixed note creation form still appearing even when logged out

--- src/components/stickyboard/note_creation/CreateNoteButton.tsx
+++ src/components/stickyboard/note_creation/CreateNoteButton.tsx
@@ -16,7 +16,7 @@ const CreateNoteButton = ({ onClick }: CreateNoteButtonProps) => {
     }
     return (
         <div
-            onClick={onClick}
+            onClick={user ? onClick : () => {}}
             onMouseDown={stopPropagation}
             className={`${PJSFontClassName} p-5 absolute overflow-hidden top-6 right-6 w-16 h-16 ${user ? 'hover:cursor-pointer hover:w-48' : 'hover:cursor-not-allowed grayscale hover:w-64'} rounded-full group shadow-lg bg-primary flex flex-row-reverse gap-6 items-center transition-all duration-300`}
         >
